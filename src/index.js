b2d64af197b97c684a46e8ebd006de41756deaaf
R.J. Freund <robert.joseph.freund@gmail.com>
2021-02-25 19:08:49 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Illustrations @ Tina Henschel: https://www.instagram.com/tina.henschel
+
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
+import './styles.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
